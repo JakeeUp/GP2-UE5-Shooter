428cc4fd22282886ada94da0ce835747e8a888ec
Jacob060701 <fjacob31@yahoo.com>
2023-04-30 22:55:59 -0500

    EnemyRun

--- Source/GP2_Shooter/GruxAnimInstance.h
+++ Source/GP2_Shooter/GruxAnimInstance.h
@@ -13,5 +13,16 @@ UCLASS()
 class GP2_SHOOTER_API UGruxAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+private:
+	/** Lateral Movement speed */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class AEnemy* Enemy;
 };
